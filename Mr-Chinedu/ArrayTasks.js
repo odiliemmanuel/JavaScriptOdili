3ab1270b742f51800c2092467a882ceee2a0f498
Odili <odiliejeh09@gmail.com>
2026-01-31 11:44:35 +0100

    project

---
+++ Mr-Chinedu/ArrayTasks.js
@@ -0,0 +1,19 @@
+
+    for(count = 0; count < 10; count ++){
+//
+//        if(array[count] % 2 == 0){
+//
+//           array[count] =  array[count] + 1;
+//        }
+//
+//        else{
+//
+//            array[count] =  array[count] - 2;
+//        }
+//    }
+//
+//   
+// return array;
+//   process.stdout.write(`${count}`);
+}
+
