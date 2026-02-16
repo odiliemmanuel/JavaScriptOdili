81d1d1f868cb66e49b6cddfa2cda34c50ab8287e
Odili <odiliejeh09@gmail.com>
2026-02-16 10:28:58 +0100

    Tasks

---
+++ Mr-Chinedu/Student.js
@@ -0,0 +1,30 @@
+student = {
+
+    name : "Khid Flash",
+    age : 16,
+    subject : "ICT",
+    grade : 38
+       
+}
+
+
+
+student.grade = "Data Processng";
+
+delete student.age;
+
+console.log(Object.keys(student))
+console.log(Object.values(student))
+
+
+
+
+
+
+
+//name
+//age,
+//grade
+//change grade
+//delete age
+//print(print)
