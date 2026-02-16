fcb8d1c243283b92e2d1b704f6e2635422eb1d0f
Odili <odiliejeh09@gmail.com>
2026-02-16 10:20:04 +0100

    Tasks

---
+++ Mr-Chinedu/Person.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')();
+
+
+let  person = {
+
+    firstName: "Barry",
+    lastName : "Allen",
+     age     :  16
+    
+    
+
+    };
+
+person.email = "khidflash@gmail.com"; 
+ 
+person.age = 17;
+
+//delete person.lastName 
+ 
+console.log(person + "\n");
+console.log(Object.values(person.lastName))
+
+
+
